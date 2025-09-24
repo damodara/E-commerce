f7cb38bbf58dad70b26e11cfc1d46fa6ab75a6bc
damodara <jay108@mail.ru>
2025-09-24 19:53:26 +0300

    part 2 - done

--- src/category.py
+++ src/category.py
@@ -7,7 +7,16 @@ class Category:
     name: str
     description: str
     products: List[Product]
+
+    # атрибуты класса (общие для всех объектов)
+    category_count = 0
+    product_count = 0
+
     def __init__(self, name: str, description: str, products: List[Product]):
         self.name = name
         self.description = description
         self.products = products
+
+        # автоматическое обновление атрибутов класса
+        Category.category_count += 1
+        Category.product_count += len(products)
